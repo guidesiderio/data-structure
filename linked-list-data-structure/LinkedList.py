6e1fe026c44b7fe5487b6608cf000bb8697f7415
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2023-06-12 22:40:45 -0300

    Add def insert_ordered()

--- linked-list-data-structure/LinkedList.py
+++ linked-list-data-structure/LinkedList.py
@@ -43,6 +43,28 @@ class LinkedList:
             current = current.next
 
         print(f"O valor {value} não foi encontrado na lista.")
+        
+
+    def insert_ordered(self, data):
+        new_node = Node(data)
+
+        # Caso a lista esteja vazia
+        if self.head is None:
+            self.head = new_node
+        # Caso o novo elemento seja menor que o primeiro elemento da lista
+        elif data < self.head.data:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+
+            # Encontrar o local correto para inserir o novo elemento
+            while current.next is not None and current.next.data < data:
+                current = current.next
+
+            new_node.next = current.next
+            current.next = new_node
+
 
     def display(self):
         if self.is_empty():
